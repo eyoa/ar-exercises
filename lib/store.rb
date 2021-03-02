40fa9e28ddb427041600547afb275cc9fe1a1311
Evelyn Yoa <yoaevelyn@gmail.com>
2021-03-02 01:16:53 +0000

    edd exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validate :must_carry_apparel
+  
+
+  def must_carry_apparel
+    if !(mens_apparel == true || womens_apparel == true)
+      errors.add(:mens_apparel, "either mens or womens apparel must be true")
+      errors.add(:womens_apparel, "either mens or womens apparel must be true")
+    end
+  end
+
 end
